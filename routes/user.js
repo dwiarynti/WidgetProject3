362ef6ee80992308e87d29ee62b7b5ae0888f3d5
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-05-30 12:29:34 +0700

    update create user

--- routes/user.js
+++ routes/user.js
@@ -2,23 +2,77 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 var db = require('./connection');
-
+var sequencedb = db.sublevel('sequencenumberuser');
 var userdb = db.sublevel('user');
 
-var user = [
+var users = [
     {
         id: 1,
         username:'dwi',
         password:'123',
-        pages: []
+        pages: [],
+        role:{}
     },
     
 ]
 router.post('/user/create', function (req, res) {
-    userdb.put('user', user, function (err) {
-        if (err) res.json(500, err);
-        else res.json({ success: true });
-    });
+
+    var id = "";
+    sequencedb.get('sequencenumberuser',function()
+    {
+         if (err)
+            if (err.message == "Key not found in database") {
+                var no = 0;
+                sequencedb.put('sequencenumberpersondevice', no, function (err, no) {
+                    if (err) res.json(500, err)
+                    else id = no + 1;
+                });
+            }
+            else {
+                res.json(500, err);
+            }
+        else
+        id = no+1;
+        if(id != "")
+        {
+        var user = {
+            id : id,
+            username: req.body.username,
+            password : req.body.password,
+            role : req.body.role,
+            pages : req.body.pages
+        }  
+        var listobj = [];
+        userdb.get('user',function(err,obj)
+        {
+            if(err)
+            if(err.message == "Key not found in database")
+            {
+               listobj.push(user);
+            }
+            else
+            {
+                res.json(500,err);
+            }
+            else
+            if(obj.lenght != 0)
+            {
+                listobj = obj;
+                listobj.push(user);
+
+            }
+            else
+            {
+                listobj.push(user);
+            }
+
+            userdb.put('user', listobj, function (err) {
+            if (err) res.json(500, err);
+            else res.json({ success: true });
+        });
+     });
+    }
+ });
 });
 
 router.post('/user/login',function(req,res)
