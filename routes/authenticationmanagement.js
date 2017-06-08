2b74ff154648ed97f7b24c82ee00cd466721e440
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-06-08 10:15:18 +0700

    add check total notif

--- routes/authenticationmanagement.js
+++ routes/authenticationmanagement.js
@@ -4,29 +4,54 @@ var router = express.Router();
 var db = require('./connection');
 
 var authmanagementdb = db.sublevel('authmanagement');
-
+var messagedb = db.sublevel('message');
 router.get('/auth/init',function(req,res)
 {
-    authmanagementdb.get('authmanagement',function(err,data)
+    var totalnotif =0;
+    var auth = false;
+    messagedb.get('message',function(err,messages)
     {
-        var auth = false;
-        if(err)
-        {
-            if (err.message == "Key not found in database") {
-               auth = true;
-               res.json({"success":true,"obj": auth})
-            }
-            else {
-                res.json(500, err);
-            }
-        }
+     if(err)
+     {
+        if(err.message == "Key not found in database")
+         {
+             totalnotif =0;
+         }
         else
-        {
-           
-            res.json({"success":true,"obj": data})
-
+         {
+            res.json(500,err);
         }
-    })
+      }
+      else
+      {
+          if(req.session.role == "User")
+          {
+            totalnotif = messages.length;
+          }
+          else
+          {
+              totalnotif = messages.length;
+          }
+          authmanagementdb.get('authmanagement',function(err,data)
+          {
+            if(err)
+            {
+                if (err.message == "Key not found in database") {
+                auth = true;
+                res.json({"success":true,"obj": auth,"totalnotif": totalnotif})
+                }
+                else {
+                    res.json(500, err);
+                }
+            }
+            else
+            {
+                res.json({"success":true,"obj": data,"totalnotif": totalnotif})
+            }
+          });
+       
+      }
+    });
 })
 
 
@@ -39,5 +64,8 @@ router.post('/auth/update',function(req,res)
         else
         res.json({"success":true})
     })
-})
+});
+
+
+
 module.exports = router;
