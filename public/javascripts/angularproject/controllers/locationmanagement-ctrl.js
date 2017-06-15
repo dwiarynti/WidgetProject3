9a17c9b54cac21bdca46371540eb196a65110086
Rjuzariah <riajuzariah@gmail.com>
2017-06-15 10:02:25 +0700

    add template

---
+++ public/javascripts/angularproject/controllers/locationmanagement-ctrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('locationmanagementcontroller',
+    ['$scope',
+        function ($scope,) {
+
+        }
+    ]);
---
+++ public/javascripts/angularproject/controllers/peoplemanagement-ctrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('peoplemanagementcontroller',
+    ['$scope',
+        function ($scope,) {
+
+        }
+    ]);
