a3a0e4e12a9944dc50ff36fe384b778dbd3fa7d6
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-06-21 14:58:02 +0700

    add create and getall widget

--- routes/widgetmanagement.js
+++ routes/widgetmanagement.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var db = require('./connection');
 var person = require('./person');
 var persondb = db.sublevel('person');
-
+var widgetdb = db.sublevel('widgetmanagement');
+var sequencedb = db.sublevel('sequencenumberwidget');
 router.get('/widgetmanagement/datasource',function(req,res)
 {
 
@@ -53,10 +54,95 @@ router.get('/widgetmanagement/datasource',function(req,res)
 });
 });
 
-router.get('/widgetmanagement/datasourcedetail',function(req,res)
+router.post('/widgetmanagement/create',function(req,res)
 {
   
+  var listobj = [];
+  var generateid = "";
+  sequencedb.get('sequencenumberwidget',function(err,id)
+  {
+    if(err)
+        {
+            if(err.message == "Key not found in database")
+            {
+                generateid = 1;
+            }
+            else
+            {
+                res.json(500,err);
+            }
+        }
+        else{
+            generateid = id+1;
+        }
+
+        var widget = {
+            "euid": generateid,
+            "appname": req.body.appname,
+            "widget" : req.body.widget
+        }
+        widgetdb.get('widgetmanagement',function(err,widgetsetting)
+        {
+            if(err)
+            {
+                    if(err.message == "Key not found in database")
+                    {
+                        listobj.push(widget);
+                    }
+                    else
+                    {
+                        res.json(500,err);
+                    }
+            }
+            else
+            {
+                if(widgetsetting.length > 0)
+                {
+                    listobj = widgetsetting;
+                    listobj.push(widget)
+                }
+                else
+                {
+                    listobj.push(widget);
+                }
+
+            }
+            widgetdb.put('widgetmanagement',listobj,function(err)
+            {
+                if(err)
+                res.json(500,err);
+                else
+                res.json({"success":true})
+            });
+
+
+            
+        });
+  });
     
+});
+
+router.get('/widgetmanagement/getall',function(req,res)
+{
+    widgetdb.get('widgetmanagement',function(err,result)
+    {
+        if(err)
+        {
+            if(err.message == "Key not found in database")
+            {
+                res.json({"success":true,"obj": []})
+            }
+            else
+            {
+                res.json(500,err);
+            }
+        }
+        else
+        {
+            res.json({"success":true,"obj": result})
+        }
+    })
+
 });
 
 
